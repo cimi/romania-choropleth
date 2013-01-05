8c6a068a855f73e2c29b21475a8fb209e3299446
Alex Ciminian <alex.ciminian@gmail.com>
2013-01-05 17:58:19 +0200

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,15 @@
+require.config({
+  shim: {
+  },
+
+  paths: {
+    hm: 'vendor/hm',
+    esprima: 'vendor/esprima',
+    jquery: 'vendor/jquery.min'
+  }
+});
+ 
+require(['app'], function(app) {
+  // use app here
+  console.log(app);
+});
